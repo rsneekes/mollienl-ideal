e6f357cc21d832556813e07757652a611a00e1e9
Raymond Sneekes <raymond@sneek.es>
2011-05-08 15:08:08 +0200

    Only require engine for rails

--- lib/mollie-ideal.rb
+++ lib/mollie-ideal.rb
@@ -4,10 +4,10 @@ require 'rexml/document'
 require 'rails'
 
 require 'mollie-ideal/config'
-require 'mollie-ideal/engine'
 
 require 'mollie-ideal/api'
 require 'mollie-ideal/bank'
 require 'mollie-ideal/payment_request'
 require 'mollie-ideal/payment_result'
 
+require 'mollie-ideal/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
